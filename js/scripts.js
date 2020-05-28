790ba876be427ce611ce945793c7cfbcda48a45b
Jithvan <jithvan@gmail.com>
2020-05-28 02:39:03 -0400

    Scrolling Changes
    - Added Smooth Scroll
    - Added Back2Top Button
    - Added Uses Titles

--- js/scripts.js
+++ js/scripts.js
@@ -86,3 +86,22 @@ function mobileMenu() {
     x.className = "topnav";
   }
 }
+
+/*Scroll to top when arrow up clicked BEGIN*/
+$(window).scroll(function() {
+    var height = $(window).scrollTop();
+    if (height > 100) {
+        $('#back2Top').fadeIn();
+    } else {
+        $('#back2Top').fadeOut();
+    }
+});
+$(document).ready(function() {
+    $("#back2Top").click(function(event) {
+        event.preventDefault();
+        $("html, body").animate({ scrollTop: 0 }, "slow");
+        return false;
+    });
+
+});
+ /*Scroll to top when arrow up clicked END*/
